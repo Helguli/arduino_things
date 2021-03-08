d54352fda010289fc770c561c4f5bb1eed0c4887
Helguli <helguli@yahoo.com>
2021-03-08 16:54:03 +0100

    Gombok detektálása

--- button_test/src/button_test.ino
+++ button_test/src/button_test.ino
@@ -5,5 +5,23 @@ void setup() {
 
 void loop() {
     int sensor_value = analogRead(A0);
-    Serial.println(sensor_value);
+    if (sensor_value < 30) {
+        Serial.println("Button #1");
+    } else if (sensor_value < 50) {
+        Serial.println("Button #2");
+    } else if (sensor_value < 80) {
+        Serial.println("Button #3");
+    } else if (sensor_value < 102) {
+        Serial.println("Button #4");
+    } else if (sensor_value < 126) {
+        Serial.println("Button #5");
+    } else if (sensor_value < 146) {
+        Serial.println("Button #6");
+    } else if (sensor_value < 168) {
+        Serial.println("Button #7");
+    } else if (sensor_value < 190) {
+        Serial.println("Button #8");
+    } else {
+        Serial.println("No button pressed.");
+    }
 }
