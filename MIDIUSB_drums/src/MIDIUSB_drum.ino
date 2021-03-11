4a4a8a89eaf73b83cdda84d3450b21d7d17dff01
Helguli <helguli@yahoo.com>
2021-03-11 20:36:27 +0100

    Sampling timer for the piezo sensors added

--- MIDIUSB_drums/src/MIDIUSB_drum.ino
+++ MIDIUSB_drums/src/MIDIUSB_drum.ino
@@ -2,26 +2,36 @@
 #include "DueTimer.h"
 #include "U8g2lib.h"
 
-#define NUM_OF_ALL_DRUMS 8
+#define NUM_OF_ALL_DRUMS 19
 #define NUM_OF_ANALOG_DRUMS 4
-#define AD_THRESHOLD 200
+#define AD_THRESHOLD 300
+#define AD_THRESHOLD_LOW 30
 #define DELAY 1000
 #define BUTTON_PIN_0 A10
 #define BUTTON_PIN_1 A11
 #define LED_PIN 46
 #define EVENT_DELAY 50
 #define SWITCH_SAMPLING_TIME 10000
+#define PIEZO_SAMPLING_TIME 1000
 #define BUFFER_MAX_SIZE 8
 #define TEMPO_SCALE 15000000
+#define FIRST_LINE 7
+#define SECOND_LINE 16
+#define THIRD_LINE 25
+#define FOURTH_LINE 34
+#define FIFTH_LINE 43
+#define SIXTH_LINE 52
+#define LAST_LINE 66
+#define MAX_PAGE 7
 
 
 //int16_t ad_value;
 //int16_t prev_ad_value;
 uint16_t signal_detected[NUM_OF_ANALOG_DRUMS] = {0, 0, 0, 0};
 int16_t drum_pins[NUM_OF_ANALOG_DRUMS] = {A0, A1, A2, A3};
-uint8_t drum_notes_all[NUM_OF_ALL_DRUMS] = {36, 38, 42, 46, 41, 43, 45, 49};
-uint8_t drum_notes_analog[] = {36, 38, 42, 49};
-char *drum_names_all[NUM_OF_ALL_DRUMS] = {"Kick", "Snare", "Closed Hi-Hat", "Open Hi-Hat", "Low Tom", "Mid Tom", "High Tom", "Crash"};
+uint8_t drum_notes_all[NUM_OF_ALL_DRUMS] = {36, 37, 38, 39, 42, 46, 41, 43, 45, 49, 51, 54, 55, 56, 60, 61, 63, 75, 81};
+uint8_t drums_analog[NUM_OF_ANALOG_DRUMS] = {11, 12, 13, 18};
+const char *drum_names_all[NUM_OF_ALL_DRUMS] = {"Kick", "Side Stick", "Snare", "Clap", "Closed Hi-Hat", "Open Hi-Hat", "Low Tom", "Mid Tom", "High Tom", "Crash", "Ride", "Tambourine", "Splash", "Cowbell", "High Bongo", "Low Bongo", "Conga", "Claves", "Triangle"};
 int16_t ad_values[NUM_OF_ANALOG_DRUMS];
 int16_t prev_ad_values[NUM_OF_ANALOG_DRUMS];
 uint16_t sw_tresholds[] = {172, 430, 567, 652, 710, 753, 785, 850, 172, 430, 567, 652};
@@ -77,7 +87,7 @@ size_t sendNotes(midiEventPacket_t events[], size_t size)
 	return MidiUSB.write(data, 4 * size);
 }
 
-void switchSample()
+void switchSampling()
 {
     int sensor_value = analogRead(BUTTON_PIN_0);
     int i = 0;
@@ -113,6 +123,32 @@ void switchSample()
     {
         button_no = 0;
         button_pressed = 0;
+    }
+}
+
+void piezoSampling()
+{
+    //TODO
+    for (int i = 0; i < 1/*NUM_OF_ANALOG_DRUMS*/; i++) 
+    {
+        int16_t prev_ad_value = ad_values[i];
+        int16_t ad_value = analogRead(drum_pins[i]);
+        prev_ad_values[i] = prev_ad_value;
+        ad_values[i] = ad_value;
+        if (ad_value > AD_THRESHOLD && prev_ad_value <= ad_value && !signal_detected[i])
+        {
+            signal_detected[i]++;
+        }
+        if (signal_detected[i] == 1 && prev_ad_value > ad_value)
+        {
+            uint8_t velocity = prev_ad_value >> 3;
+            noteOn(drum_notes_all[drums_analog[i]], velocity);
+            signal_detected[i]++;
+        }
+        if (signal_detected[i] && ad_value < AD_THRESHOLD_LOW)
+        {
+            signal_detected[i] = 0;
+        }
     }
 }
 
@@ -180,56 +216,145 @@ void changeTempo(uint8_t up)
 
 void draw()
 {
-    u8g2.setCursor(0, 8);
+    u8g2.setFont(u8g2_font_5x8_tf);
+    u8g2.setCursor(0, FIRST_LINE);
     u8g2.print(tempo);
-    u8g2.setCursor(30, 8);
+    u8g2.setCursor(20, FIRST_LINE);
     u8g2.print(drum_names_all[selected_drum]);
-    u8g2.setCursor(110, 8);
-    u8g2.print(fragment_no);
-    u8g2.setCursor(0, 20);
+    u8g2.setCursor(92, FIRST_LINE);
+    u8g2.print("Beat: ");
+    u8g2.print(((fragment_no >> 2) + 1));
+    u8g2.setCursor(0, SECOND_LINE);
     switch (page)
     {
         case 0:
-            u8g2.print(" Drum: <");
+            u8g2.print("# Drum: <");
             u8g2.print(drum_names_all[selected_drum]);
             u8g2.print(">");
-            u8g2.setCursor(0, 32);
+            u8g2.setCursor(0, THIRD_LINE);
             u8g2.print("Measure: ");
             u8g2.print(measure_no + 1);
-            u8g2.setCursor(0, 44);
+            u8g2.setCursor(0, FOURTH_LINE);
             u8g2.print("Tempo: ");
             u8g2.print(tempo);
+            u8g2.setCursor(0, FIFTH_LINE);
+            u8g2.print("Drum 1: ");
+            u8g2.print(drum_names_all[drums_analog[0]]);
+            u8g2.setCursor(0, SIXTH_LINE);
+            u8g2.print("Drum 2: ");
+            u8g2.print(drum_names_all[drums_analog[1]]);
             break;
         case 1:
             u8g2.print("Drum: ");
             u8g2.print(drum_names_all[selected_drum]);
-            u8g2.setCursor(0, 32);
-            u8g2.print(" Measure: <");
+            u8g2.setCursor(0, THIRD_LINE);
+            u8g2.print("# Measure: <");
             u8g2.print(measure_no + 1);
             u8g2.print(">");
-            u8g2.setCursor(0, 44);
+            u8g2.setCursor(0, FOURTH_LINE);
             u8g2.print("Tempo: ");
             u8g2.print(tempo);
+            u8g2.setCursor(0, FIFTH_LINE);
+            u8g2.print("Drum 1: ");
+            u8g2.print(drum_names_all[drums_analog[0]]);
+            u8g2.setCursor(0, SIXTH_LINE);
+            u8g2.print("Drum 2: ");
+            u8g2.print(drum_names_all[drums_analog[1]]);
             break;
         case 2:
             u8g2.print("Drum: ");
             u8g2.print(drum_names_all[selected_drum]);
-            u8g2.setCursor(0, 32);
+            u8g2.setCursor(0, THIRD_LINE);
             u8g2.print("Measure: ");
             u8g2.print(measure_no + 1);
-            u8g2.setCursor(0, 44);
-            u8g2.print(" Tempo: <");
+            u8g2.setCursor(0, FOURTH_LINE);
+            u8g2.print("# Tempo: <");
             u8g2.print(tempo);
+            u8g2.print(">");
+            u8g2.setCursor(0, FIFTH_LINE);
+            u8g2.print("Drum 1: ");
+            u8g2.print(drum_names_all[drums_analog[0]]);
+            u8g2.setCursor(0, SIXTH_LINE);
+            u8g2.print("Drum 2: ");
+            u8g2.print(drum_names_all[drums_analog[1]]);
+            break;
+        case 3:
+            u8g2.print("Measure: ");
+            u8g2.print(measure_no + 1);
+            u8g2.setCursor(0, THIRD_LINE);
+            u8g2.print("Tempo: ");
+            u8g2.print(tempo);
+            u8g2.setCursor(0, FOURTH_LINE);
+            u8g2.print("# Drum 1: <");
+            u8g2.print(drum_names_all[drums_analog[0]]);
+            u8g2.print(">");
+            u8g2.setCursor(0, FIFTH_LINE);
+            u8g2.print("Drum 2: ");
+            u8g2.print(drum_names_all[drums_analog[1]]);
+            u8g2.setCursor(0, SIXTH_LINE);
+            u8g2.print("Drum 3: ");
+            u8g2.print(drum_names_all[drums_analog[2]]);
+            break;
+        case 4:
+            u8g2.print("Tempo: ");
+            u8g2.print(tempo);
+            u8g2.setCursor(0, THIRD_LINE);
+            u8g2.print("Drum 1: ");
+            u8g2.print(drum_names_all[drums_analog[0]]);
+            u8g2.setCursor(0, FOURTH_LINE);
+            u8g2.print("# Drum 2: <");
+            u8g2.print(drum_names_all[drums_analog[1]]);
+            u8g2.print(">");
+            u8g2.setCursor(0, FIFTH_LINE);
+            u8g2.print("Drum 3: ");
+            u8g2.print(drum_names_all[drums_analog[2]]);
+            u8g2.setCursor(0, SIXTH_LINE);
+            u8g2.print("Drum 4: ");
+            u8g2.print(drum_names_all[drums_analog[3]]);
+            break;
+        case 5:
+            u8g2.print("Tempo: ");
+            u8g2.print(tempo);
+            u8g2.setCursor(0, THIRD_LINE);
+            u8g2.print("Drum 1: ");
+            u8g2.print(drum_names_all[drums_analog[0]]);
+            u8g2.setCursor(0, FOURTH_LINE);
+            u8g2.print("Drum 2: ");
+            u8g2.print(drum_names_all[drums_analog[1]]);
+            u8g2.setCursor(0, FIFTH_LINE);
+            u8g2.print("# Drum 3: <");
+            u8g2.print(drum_names_all[drums_analog[2]]);
+            u8g2.print(">");
+            u8g2.setCursor(0, SIXTH_LINE);
+            u8g2.print("Drum 4: ");
+            u8g2.print(drum_names_all[drums_analog[3]]);
+            break;
+        case 6:
+            u8g2.print("Tempo: ");
+            u8g2.print(tempo);
+            u8g2.setCursor(0, THIRD_LINE);
+            u8g2.print("Drum 1: ");
+            u8g2.print(drum_names_all[drums_analog[0]]);
+            u8g2.setCursor(0, FOURTH_LINE);
+            u8g2.print("Drum 2: ");
+            u8g2.print(drum_names_all[drums_analog[1]]);
+            u8g2.setCursor(0, FIFTH_LINE);
+            u8g2.print("Drum 3: ");
+            u8g2.print(drum_names_all[drums_analog[2]]);
+            u8g2.setCursor(0, SIXTH_LINE);
+            u8g2.print("# Drum 4: <");
+            u8g2.print(drum_names_all[drums_analog[3]]);
             u8g2.print(">");
             break;
     }
-    u8g2.setCursor(5, 64);
+    u8g2.setFont(u8g2_font_amstrad_cpc_extended_8f);
+    u8g2.setCursor(5, LAST_LINE);
     u8g2.print("");
-    u8g2.setCursor(44, 64);
+    u8g2.setCursor(44, LAST_LINE);
     u8g2.print("");
-    u8g2.setCursor(82, 64);
+    u8g2.setCursor(82, LAST_LINE);
     u8g2.print("");
-    u8g2.setCursor(120, 64);
+    u8g2.setCursor(120, LAST_LINE);
     u8g2.print("");
 }
 
@@ -247,11 +372,11 @@ void setup()
     MidiUSB.flush();
     drum_timer.attachInterrupt(playNote).start(fragment_delay);
     DueTimer switch_timer = Timer.getAvailable();
-    switch_timer.attachInterrupt(switchSample).start(SWITCH_SAMPLING_TIME);
-    // TODO mintavételezéshez egy timert konfigurálni
+    switch_timer.attachInterrupt(switchSampling).start(SWITCH_SAMPLING_TIME);
+    DueTimer piezo_timer = Timer.getAvailable();
+    piezo_timer.attachInterrupt(piezoSampling).start(PIEZO_SAMPLING_TIME);
     u8g2.begin();
     u8g2.enableUTF8Print();
-    u8g2.setFont(u8g2_font_amstrad_cpc_extended_8f);
 }
 
 void loop()
@@ -272,34 +397,6 @@ void loop()
                 draw();
             } while (u8g2.nextPage());
     }
-    /*
-    for (int i = 0; i < NUM_OF_DRUMS; i++) 
-    {
-        int16_t prev_ad_value = ad_values[i];
-        int16_t ad_value = analogRead(drum_pins[i]);
-        ad_value = abs(ad_value);
-        prev_ad_values[i] = prev_ad_value;
-        ad_values[i] = ad_value;
-        if (ad_value > AD_THRESHOLD && prev_ad_value <= ad_value && signal_detected[i] == 0) 
-        {
-            signal_detected[i] = 2000;
-        }
-        if (signal_detected[i] > 1500 && prev_ad_value > ad_value) 
-        {
-            uint8_t velocity = prev_ad_value/8;
-            noteOn(9, drum_notes[i], velocity);
-//            Serial.print(prev_ad_value);
-//            Serial.print(":");
-//            Serial.println(signal_detected[i]);
-            MidiUSB.flush();
-            signal_detected[i] = 1500;
-        }
-        //Serial.println(ad_value);
-        if (signal_detected[i]) 
-        {
-            signal_detected[i] = signal_detected[i] - 1;
-        }
-    }*/
     if (button_no && button_pressed >= 5)
     {
         // TODO handle button events
@@ -341,11 +438,11 @@ void loop()
         }
         else if (button_no == 11)
         {
-            page = (page + 2) % 3;
+            page = (page + MAX_PAGE - 1) % MAX_PAGE;
         }
         else if (button_no == 12)
         {
-            page = (page + 1) % 3;
+            page = (page + 1) % MAX_PAGE;
         }
         button_no = 0;
     }
